e53b166786a154acc2b962e46d6264393c5d106e
Aaron Lin <aaronlin74@gmail.com>
2021-11-25 21:58:19 +1100

    implement navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/'></Route>
         </Routes>
+        <Footer />
       </Router>
     </>
   )
