d07622e9286b81dbc9039e28b955ced639800cdd
Aaron Lin <aaronlin74@gmail.com>
2021-11-26 21:03:27 +1100

    implement the hero section, set the home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Home from './components/Pages/Home'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/'></Route>
+          <Route path='/' element={<Home />}></Route>
         </Routes>
         <Footer />
       </Router>
