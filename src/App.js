7b47e6f663afe3b9c5749d2acce71d3349476ee3
Aaron Lin <aaronlin74@gmail.com>
2021-12-06 15:01:02 +1100

    Add sample pages to the app and update the css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './components/Pages/Home'
+import Services from './components/Pages/Services'
+import Products from './components/Pages/Products'
+import SignUp from './components/Pages/SignUp'
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />}></Route>
+          <Route path='/services' element={<Services />}></Route>
+          <Route path='/products' element={<Products />}></Route>
+          <Route path='/sign-up' element={<SignUp />}></Route>
         </Routes>
         <Footer />
       </Router>
