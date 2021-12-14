0db702c308c000b5e5d2f4ce3311b892888b73e5
Aaron Lin <aaronlin74@gmail.com>
2021-12-14 23:02:28 +1100

    Add Reviews component to the home page

---
+++ src/components/Reviews.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
+import { FaQuoteRight } from 'react-icons/fa'
+import './Reviews.css'
+
+const Reviews = () => {
+  const [reviews, setReviews] = useState(null)
+  const [index, setIndex] = useState(0)
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  // Fetch the reviews from the server
+  const fetchReivews = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/reviews')
+      if (!response.ok) {
+        throw new Error('Network response was not OK')
+      }
+      const data = await response.json()
+      setReviews(data.reviews)
+      setIsLoaded(true)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  // Call fetch reviews when mounted
+  useEffect(() => {
+    fetchReivews()
+  }, [])
+
+  useEffect(() => {
+    if (isLoaded) {
+      const lastIndex = reviews.length - 1
+
+      // Boundary condition, set to loop
+      if (index < 0) {
+        setIndex(lastIndex)
+      } else if (index > lastIndex) {
+        setIndex(0)
+      }
+    }
+  }, [index, reviews])
+
+  // Let the reivews keep rolling
+  useEffect(() => {
+    const slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 4000)
+    return () => clearInterval(slider)
+  })
+
+  return (
+    <section className='reviews'>
+      <h1 className='rev-title'>/ Reviews</h1>
+      <div className='reviews-center'>
+        {isLoaded &&
+          reviews.map((review, ind) => {
+            const { id, image, name, title, quote } = review
+            let slideClass = 'nextSlide'
+            if (ind === index) {
+              slideClass = 'activeSlide'
+            }
+            if (
+              ind === index - 1 ||
+              (index === 0 && ind === reviews.length - 1)
+            ) {
+              slideClass = 'lastSlide'
+            }
+            return (
+              <article key={id} className={slideClass}>
+                <img src={image} alt={name} className='person-img' />
+                <h4>{name}</h4>
+                <p className='title'>{title}</p>
+                <p className='text'>{quote}</p>
+                <FaQuoteRight className='icon' />
+              </article>
+            )
+          })}
+
+        <button className='prev' onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className='next' onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  )
+}
+
+export default Reviews
