1cfe3d2d23be48fb96793c39e2a5b4c6129bd836
Aaron Lin <aaronlin74@gmail.com>
2021-12-14 16:29:57 +1100

    Update hero section, add toggle button

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Button from './Button'
-import { FaPlayCircle } from 'react-icons/fa'
+import { FaPlayCircle, FaPauseCircle } from 'react-icons/fa'
 import './HeroSection.css'
 
 const HeroSection = () => {
+  const videoRef = useRef(null)
+  const [isPlay, setIsPlay] = useState(true)
+
+  // toggle the play or pause of the hero video
+  const pausePlayVideo = () => {
+    setIsPlay(!isPlay)
+    if (videoRef.current.paused || videoRef.current.ended)
+      videoRef.current.play()
+    else videoRef.current.pause()
+  }
+
+  // redirect to the youtube on a new tab
+  const redirect = () => {
+    window.open('https://www.youtube.com/watch?v=MhhAox6Zei8', '_blank')
+  }
   return (
     <div className='hero-container'>
-      <video src='/videos/animals.mp4' autoPlay loop muted />
+      <video ref={videoRef} autoPlay loop muted poster='/images/panda.jpg'>
+        <source src='/videos/animals.mp4' type='video/mp4' />
+        <source src='/videos/animals.webm' type='video/webm' />
+      </video>
+      <div className='toggle-hero-video'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--medium'
+          onClick={pausePlayVideo}
+        >
+          {isPlay ? <FaPauseCircle /> : <FaPlayCircle />}
+        </Button>
+      </div>
       <h1>Adventure awaits</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
@@ -18,13 +46,14 @@ const HeroSection = () => {
         >
           GET STARTED
         </Button>
+
         <Button
           className='btns'
           buttonStyle='btn--primary'
           buttonSize='btn--large'
-          path='/'
+          onClick={redirect}
         >
-          WATCH <FaPlayCircle />
+          Watch <FaPlayCircle />
         </Button>
       </div>
     </div>
