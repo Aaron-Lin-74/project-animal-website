e56e7f2f6fc3a64f7f0fa0c6f4b4a42b062a7204
Aaron Lin <aaronlin74@gmail.com>
2021-12-24 22:15:27 +1100

    Updated Button component, adding more style

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,25 +2,48 @@ import React from 'react'
 import './Button.css'
 import { Link } from 'react-router-dom'
 
-const STYLES = ['btn--primary', 'btn--outline']
+const STYLES = ['btn--primary', 'btn--outline', 'btn--danger', 'btn--confirm']
 const SIZES = ['btn--medium', 'btn--large']
 
-const Button = ({ children, type, onClick, buttonStyle, buttonSize, path }) => {
-  // if a button does not have a style, the default style is btn--primary, default size is medium
+const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+  path,
+  disabled,
+}) => {
+  // If a button does not have a style, the default style is btn--primary, default size is medium
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
     : STYLES[0]
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+
+  // If a path is provided, add Link. Otherwise, just wrapped with button.
+  if (path) {
+    return (
+      <Link to={path}>
+        <button
+          className={`btns ${checkButtonStyle} ${checkButtonSize}`}
+          onClick={onClick}
+          type={type}
+          disabled={disabled}
+        >
+          {children}
+        </button>
+      </Link>
+    )
+  }
   return (
-    <Link to={path || '/'}>
-      <button
-        className={`btns ${checkButtonStyle} ${checkButtonSize}`}
-        onClick={onClick}
-        type={type}
-      >
-        {children}
-      </button>
-    </Link>
+    <button
+      className={`btns ${checkButtonStyle} ${checkButtonSize}`}
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+    >
+      {children}
+    </button>
   )
 }
 
