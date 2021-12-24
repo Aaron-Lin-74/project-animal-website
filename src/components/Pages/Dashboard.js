c1968126a933516ab121d35290b5e87aac1f3299
Aaron Lin <aaronlin74@gmail.com>
2021-12-24 22:11:36 +1100

    Removed logout button from Dashboard

--- src/components/Pages/Dashboard.js
+++ src/components/Pages/Dashboard.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import Profile from '../Profile'
 
 const Dashboard = () => {
-  const { currentUser, logout } = useAuth()
+  const { currentUser } = useAuth()
   return (
     currentUser && (
-      <>
+      <div className='dashboard'>
         <Profile />
-        <button onClick={logout}>Log Out</button>
-      </>
+      </div>
     )
   )
 }
