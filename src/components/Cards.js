24f0d92ad80d680675f47bf3c9609f88d24f03fe
Aaron Lin <aaronlin74@gmail.com>
2021-12-27 23:05:37 +1100

    Update all the browser routes to lower case

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -13,13 +13,13 @@ const Cards = () => {
               src='images/fennec.jpg'
               text='Mammals include humans and all other animals that are warm-blooded vertebrates (vertebrates have backbones) with hair. They feed their young with milk and have a more well-developed brain.  '
               label='Mammals'
-              path='/animals/Mammal'
+              path='/animals/mammal'
             />
             <CardItem
               src='images/cockatoo.jpg'
               text={`Birds are warm-blooded vertebrates (vertebrates have backbones) and are the only animals with feathers. Although all birds have wings, a few species can't fly.`}
               label='Birds'
-              path='/animals/Bird'
+              path='/animals/bird'
             />
           </ul>
           <ul className='card-items-s'>
@@ -27,25 +27,25 @@ const Cards = () => {
               src='images/the_frilled_neck_lizard.jpg'
               text='Reptiles are cold-blooded vertebrates. (Vertebrates have backbones.) They have dry skin covered with scales or bony plates and usually lay soft-shelled eggs.'
               label='Reptiles'
-              path='/animals/Reptile'
+              path='/animals/reptile'
             />
             <CardItem
               src='images/lion_fish.jpg'
               text='Fish are vertebrates (vertebrates have backbones) that live in water. They breathe using special organs called gills.'
               label='Fish'
-              path='/animals/Fish'
+              path='/animals/fish'
             />
             <CardItem
               src='images/tree_frog.jpg'
               text='Amphibians are cold-blooded vertebrates (vertebrates have backbones) that don’t have scales. They live part of their lives in water and part on land.'
               label='Amphibians'
-              path='/animals/Amphibian'
+              path='/animals/amphibian'
             />
             <CardItem
               src='images/sea_star.jpg'
               text='Invertebrates are animals without a backbone or bony skeleton. They range in size from microscopic mites and almost invisible flies to giant squid with soccer-ball-size eyes.'
               label='Invertebrates '
-              path='/animals/Invertebrate'
+              path='/animals/invertebrate'
             />
           </ul>
         </div>
