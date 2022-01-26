80c9d5d6915bcc41fd5e81012e87a5e310848ea5
Aaron Lin <aaronlin74@gmail.com>
2022-01-26 22:27:59 +1100

    Added FAQ page and faqs data file

---
+++ src/components/FaqCard.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import { BiChevronDown, BiChevronUp } from 'react-icons/bi'
+import './FaqCard.css'
+
+const FaqCard = ({ question, answer, setActiveIdx, activeIdx, index }) => {
+  // Whether the active question index is the current question index
+  const [toggle, setToggle] = useState(index === activeIdx)
+
+  // Everytime when the active index changes, reinitialise the toggle
+  useEffect(() => {
+    setToggle(index === activeIdx)
+  }, [index, activeIdx])
+
+  return (
+    <div className='faq-card'>
+      <div
+        className={toggle ? 'card-header select' : 'card-header'}
+        onClick={() => {
+          if (index === activeIdx) {
+            setToggle(!toggle)
+          } else {
+            setActiveIdx(index)
+          }
+        }}
+      >
+        <h3>
+          {question}{' '}
+          <span className='arrow-icon'>
+            {toggle ? <BiChevronUp /> : <BiChevronDown />}{' '}
+          </span>
+        </h3>
+      </div>
+      <div className={toggle ? 'collapse show' : 'collapse'}>{answer}</div>
+    </div>
+  )
+}
+
+export default FaqCard
---
+++ src/faqs.js
@@ -0,0 +1,42 @@
+export const faqs = [
+  {
+    question: 'How can I visited the animal types with start?',
+    answer:
+      'You would need to create a new account from sign up page if you do not have one. If you have an account, you need to login in order to visit the premium content.',
+  },
+  {
+    question: 'Can I update my profile?',
+    answer:
+      'Yes, once you have logged in, simply click the username on the right hand side of the navbar to update your profile.',
+  },
+  {
+    question: 'Can I delete my account?',
+    answer:
+      'Yes, you can delete your account at any time. You can find the delete button on the profile page. ',
+  },
+  {
+    question: 'How can I leave the feedback?',
+    answer:
+      'We value your feedback. You can leave your feedback by submitting the feedback form on the contact page. You simple leave your name, email address, and the message that you would like to let us know.',
+  },
+  {
+    question: 'What information will I receive if I subscribe the news letter?',
+    answer:
+      'Unfortunately, this is for demonstration only at this stage. We simply store your email address in our backend database so you would not receive any actual news letter.',
+  },
+  {
+    question: 'Can I follow the social media from the footer?',
+    answer:
+      'Unfortunately, this is for demonstration only at this stage. Clicking the social media icon would only lead you to their home page. We are looking to create the actual social media home pages and update the links in the future.',
+  },
+  {
+    question: 'Where can I find the terms of use?',
+    answer:
+      'You can find the terms of use button at the bottom right hand side of the footer.',
+  },
+  {
+    question: 'Can I watch the full hero video?',
+    answer:
+      'Simply click the watch button on the home page, a new tab of the Youtube will pop up.',
+  },
+]
