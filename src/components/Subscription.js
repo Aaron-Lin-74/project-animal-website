eb81eab395d40a33bf08ead0bee6110635594346
Aaron Lin <aaronlin74@gmail.com>
2022-01-28 15:42:22 +1100

    Added subscription functionality

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -1,23 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Button from './Button'
 import './Subscription.css'
+
 const Subscription = () => {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState('')
+  const [error, setError] = useState('')
+
+  function showError(message) {
+    setError(message)
+    setTimeout(() => setError(''), 5000)
+  }
+  async function handleSubmit(e) {
+    e.preventDefault()
+    try {
+      const response = await fetch('/api/subscription', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      })
+      const data = await response.json()
+      if (!response.ok) {
+        showError(data.message)
+        return
+      }
+      navigate('/thank-you/subscription')
+    } catch (err) {
+      showError(err)
+    }
+  }
   return (
     <section className='subscription'>
+      {error && <div className='error-info'>{error}</div>}
       <h3 className='subscription-heading'>
         Join the Learn Animals newsletter to receive our latest updates
       </h3>
       <p className='subscription-text'>You can unsubscribe at any time.</p>
       <div className='subscription-input-wrap'>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type='email'
             name='email'
             className='subscription-input'
             placeholder='Your Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
-          <Button buttonStyle='btn--outline' buttonSize='btn--medium'>
+          <Button
+            buttonStyle='btn--outline'
+            buttonSize='btn--medium'
+            type='submit'
+          >
             Join Now
           </Button>
         </form>
