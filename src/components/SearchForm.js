c8fb31afb8b032318bc1c82c12c65c2f169fface
Aaron Lin <aaronlin74@gmail.com>
2022-02-03 10:36:07 +1100

    Dynamically show animal type

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { useGlobalContext } from '../contexts/AppContext'
+import { useParams } from 'react-router-dom'
 import { FcSearch } from 'react-icons/fc'
 import './SearchForm.css'
 
 const SearchForm = () => {
   const { searchTerm, setSearchTerm } = useGlobalContext()
+  // Change the animal type from plural to single
+  const { animalType } = useParams()
+  const type = animalType.endsWith('s') ? animalType.slice(0, -1) : animalType
   const handleSubmit = (e) => {
     e.preventDefault()
   }
@@ -13,7 +17,7 @@ const SearchForm = () => {
       <form className='search-form' onSubmit={handleSubmit}>
         <div className='search-form-control'>
           <label htmlFor='name'>
-            Search your favorite animal
+            Search your favorite {type}
             <FcSearch />
           </label>
           <input
