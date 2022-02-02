41678e79cd94fa3023596e3a48a4239e31e31775
Aaron Lin <aaronlin74@gmail.com>
2022-02-03 10:26:23 +1100

    Added loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Loading.css'
+
+const Loading = () => {
+  return (
+    <div className='loading'>
+      Loading...
+      <img src='/images/loading.jpg' alt='loading' />
+    </div>
+  )
+}
+
+export default Loading
