86bc8e15932dd91f556d0285b627367453ab5fa1
Aaron Lin <aaronlin74@gmail.com>
2022-02-04 21:56:14 +1100

    Reuse card item component to replace the animal card

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './CardItem.css'
 
 const CardItem = (props) => {
   return (
     <>
       <li className='card-item'>
         <Link className='card-item-link' to={props.path}>
-          <figure className='card-item-img-wrap' data-category={props.label}>
+          <figure
+            className={`card-item-img-wrap ${props.class ? props.class : ''}`}
+            data-category={props.label}
+          >
             <img className='card-item-img' src={props.src} alt='Animal' />
           </figure>
           <div className='card-item-info'>
