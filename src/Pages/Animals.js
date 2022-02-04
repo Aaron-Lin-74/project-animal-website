64c2c2e4b437c4cbd10cbb94366b8037e2ea13a1
Aaron Lin <aaronlin74@gmail.com>
2022-02-04 22:43:34 +1100

    Fixed the search bar not reset when routes to other animal type

--- src/Pages/Animals.js
+++ src/Pages/Animals.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
+import { useGlobalContext } from '../contexts/AppContext'
+import { useParams } from 'react-router-dom'
 
 const Animals = () => {
+  // Reset the search bar to empty when routes to other animal type
+  const { setSearchTerm } = useGlobalContext()
+  const { animalType } = useParams()
+  useEffect(() => {
+    setSearchTerm('')
+  }, [animalType, setSearchTerm])
+
   return (
     <main className='animals'>
       <Outlet />
